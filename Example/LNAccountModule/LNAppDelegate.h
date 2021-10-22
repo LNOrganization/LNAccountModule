4cdc9323d5229bbbea21975ef574b4123895c453
dongjianxiong1 <dongjianxiong1@kingsoft.com>
2021-10-22 22:52:18 +0800

    Initial commit

---
+++ Example/LNAccountModule/LNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LNAppDelegate.h
+//  LNAccountModule
+//
+//  Created by dongjianxiong on 10/22/2021.
+//  Copyright (c) 2021 dongjianxiong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LNAccountModule/LNViewController.h
@@ -0,0 +1,13 @@
+//
+//  LNViewController.h
+//  LNAccountModule
+//
+//  Created by dongjianxiong on 10/22/2021.
+//  Copyright (c) 2021 dongjianxiong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface LNViewController : UIViewController
+
+@end
